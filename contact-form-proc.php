3aa1340d054a11484a8536dca627196e74acca7b
sumit rawat <sr674@afsconnect1.njit.edu>
2016-12-22 16:15:24 -0500

    php file

---
+++ contact-form-proc.php
@@ -0,0 +1,20 @@
+<?php
+if(empty($_POST['submit']))
+{
+    echo"Form is not submitted";
+    exit;
+}
+
+if(empty($_POST["fullname"]) ||
+    empty($_POST["email"]))
+    {
+        echo"Please fill the form";
+            exit;
+    }
+$name= $_POST["fullname"];
+$email = $_POST["email"];
+
+mail('sr674@njit.edu', 'New form submission', "New form submission: Name: $name, Email:$email");
+
+header('Location: thank-you.html');
+?>
